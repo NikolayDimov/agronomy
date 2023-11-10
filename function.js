69da410e050e0a535f4b7d22d63ded61fbc49ea2
Nikolay Dimov <dimovv.nikolay@gmail.com>
2023-11-10 17:36:50 +0200

    main udate

--- function.js
+++ function.js
@@ -3,12 +3,12 @@ export default function sum(a, b, c) {
     return result;
 }
 
-<<<<<<< HEAD
-export default function multiolied(a, b) {
+export default function multiplied(a, b) {
     let result = a * b;
-=======
+    return result;
+}
+
 export default function minus(a, b) {
     let result = a - b;
->>>>>>> 2de950e1e48c3226387e4a7a8a837415694a62f4
     return result;
 }
