4f5623e5b383b3384d23268f866f1ca34cf0d766
NikolayDimov <114500017+NikolayDimov@users.noreply.github.com>
2023-11-10 17:34:04 +0200

    new li in index and new func (#3)

--- function.js
+++ function.js
@@ -3,7 +3,12 @@ export default function sum(a, b, c) {
     return result;
 }
 
+<<<<<<< HEAD
 export default function multiolied(a, b) {
     let result = a * b;
+=======
+export default function minus(a, b) {
+    let result = a - b;
+>>>>>>> 2de950e1e48c3226387e4a7a8a837415694a62f4
     return result;
 }
